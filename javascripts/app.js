ffc32c7515620189ab5418c08c80ec68712b2656
Marcelino Llano <hello@marcelinollano.com>
2014-01-14 22:15:41 +0100

    Sorry Miguel but I hated the names

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,9 +1,9 @@
 // contents of main.js:
 require.config({
-  //By default load any module IDs from js/lib
-  baseUrl: 'js/lib',
+  //By default load any module IDs from javascripts/lib
+  baseUrl: 'javascripts/lib',
   //except, if the module ID starts with "app",
-  //load it from the js/app directory. paths
+  //load it from the javascripts/app directory. paths
   //config is relative to the baseUrl, and
   //never includes a ".js" extension since
   //the paths config could be for a directory.
